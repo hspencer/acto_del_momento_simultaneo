8482ec118ddd0b3947b5778cc2520f8bc69c5d93
hspencer <herbert.spencer@gmail.com>
2020-07-13 14:27:27 -0400

    funcionando ;)

--- sketch.js
+++ sketch.js
@@ -1,61 +1,77 @@
 let sketch; // el objeto canvas
 let data; // los datos tomados del JSON
+let notes = [];
+let w;
 
-
-// esta es la clase para construir los objetos a partir del JSON
 class Note {
   constructor(lat, lon, title, text, author) {
-    this.x = map(lat, -90, 90, 0, width); // data.query.results.[].printouts.Posición.Object.lat
-    this.y = map(lon, -90, 90, 0, height); // data.query.results.[].printouts.Posición.Object.lon
-    this.title = title; //data.query.results.[].fulltext
-    this.text = text; //data.query.results.[].printouts.Nota
-    this.autor = author; //data.query.results.[].printouts.Autor.Object.fulltext
+    this.x = map(lat, -33.5, -32.5, 0, w); 
+    this.y = map(lon, -72, -71, 0, 400); 
+    this.title = title; 
+    this.text = text; 
+    this.author = author; 
+    this.radius = map(this.text.length, 0, 50, 0, 30);
+    this.over = false;
+  }
+  
+    rollover(px, py) {
+    let d = dist(px, py, this.x, this.y);
+    this.over = d < this.radius;
   }
 
   display() {
-    ellipse(x, y, 10, 10);
+    ellipse(this.x, this.y, this.radius, this.radius);
+    if(this.over){
     textAlign(CENTER);
-    text(autor, 0, 50);
+    text(this.author, this.x, this.y + 20);}
   }
 }
 
 
-// precarga del JSON antes de construir el canvas
 function preload() {
+  w = document.getElementById("p5").offsetWidth;
   let url = "https://wiki.ead.pucv.cl/api.php?action=ask&format=json&maxlag=2000&uselang=user&errorformat=bc&query=[[Categor%C3%ADa:Acto%20del%20momento%20simult%C3%A1neo]]|%3FNota|%3FAutor|%3FPosici%C3%B3n|%3FImagen";
-  httpGet(url, 'jsonp', false, function(response) {
-    data = response;
-  });
+  data = loadJSON(url, gotData, 'jsonp');
+  
+}
+
+
+function gotData(response) {
+  for (let key in data.query.results) {
+    let thisResult = data.query.results[key];
+    let lat = thisResult.printouts['Posición'][0].lat;
+    let lon = thisResult.printouts['Posición'][0].lon;
+    let autor = thisResult.printouts['Autor'][0].fulltext;
+    let title = thisResult.fulltext;
+    let t = thisResult.printouts['Nota'][0];
+    let thisNote = new Note(lat, lon, title, t, autor);
+    notes.push(thisNote);
+    print("lat = "+lat+"\n"
+          +"lon = "+lon+"\n"
+          +"title = "+title+"\n"
+          +"t = "+t+"\n"
+          +"autor = "+autor+"\n"
+         );
+  }
 }
 
 function setup() {
-  sketch = createCanvas(document.getElementById("p5").offsetWidth, 400);
+  sketch = createCanvas(w, 400);
   sketch.parent('p5');
-  background("pink");
-  
-  // acá deberíamos pasar los datos a los objetos de class Note
-  let notes  = []; 
-  for ( let key in data.query.results ){
-    let thisResult = data.query.results[ key ];
-    let thisNote = new Note( 
-      thisResult.printouts['Posición'][0].lat,
-      thisResult.printouts['Posición'][0].lon,
-      thisResult.fulltext,
-      thisResult.printouts['Autor'][0].fulltext
-    );
-    notes.push( thisNote );
-  }
-
-  // ...
 }
 
 function windowResized() {
-  sketch = createCanvas(document.getElementById("p5").offsetWidth, 400);
+  sketch = createCanvas(w, 400);
   sketch.parent('p5');
 }
 
 function draw() {
 
+  background("pink");
+  for (let i = 0; i < notes.length; i++) {
+    notes[i].display();
+    notes[i].rollover(mouseX, mouseY);
+  }
 }
 
 function keyTyped() {
